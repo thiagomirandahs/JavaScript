da9f3b80105d60eaa98c45467192b1cf2afb76fc
Aluno <aluno@naverecife.local>
2024-04-29 20:48:19 -0300

    inicio

---
+++ aula.js
@@ -0,0 +1,14 @@
+var entrada = require('readline-sync');
+
+var nome = entrada.question("Qual seu nome? ")
+var num1 = parseFloat(entrada.question(' Digite o primeiro numero: '));
+
+var num2 = parseFloat(entrada.question('Digite o segundo numero: '));
+
+var num3 = parseFloat(entrada.question('Digite o terceiro numero: '));
+
+var soma = (num1 + num2 + num3);
+var media = (soma / 3).toFixed(2);
+
+console.log("Bom", nome, ", Sua média é :" + media);
+
