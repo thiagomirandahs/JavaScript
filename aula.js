0217eb45171a4cd8d27de7fdd6798a3704897a37
Aluno <aluno@naverecife.local>
2024-04-29 21:00:04 -0300

    att

--- aula.js
+++ aula.js
@@ -10,5 +10,6 @@ var num3 = parseFloat(entrada.question('Digite o terceiro numero: '));
 var soma = (num1 + num2 + num3);
 var media = (soma / 3).toFixed(2);
 
-console.log("Bom", nome, ", Sua média é :" + media);
+
+console.log(`Bom ${nome}, sua média é: ${media}`);
 
